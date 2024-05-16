2ffcd8d1fae6c97b1f5dd4107ede1d6f72fb83ca
Azael H. <166276745+Azael-Hue@users.noreply.github.com>
2024-05-16 11:23:41 -0700

    Prevent form submission when add button is clicked

--- ts/main.ts
+++ ts/main.ts
@@ -1,15 +1,27 @@
 /** Represent an individual product for sale */
 class Product {
-    
     /** The name of the product */
     title: string;
-
     /** The retail price of the product */
     price: number;
 
-    constructor (title: string, price: number) {
+    constructor(title: string, price: number) {
         this.title = title;
         this.price = price;
 
     }
+}
+
+window.onload = function () {
+    // Declare a variable to hold  the submit button element
+    let submitButton: HTMLButtonElement = document.querySelector('button[type=submit]');
+
+    // Add a click event listener to the submit button
+    submitButton.addEventListener('click', (event: Event) => {
+        // prevent the default form submission action
+        event.preventDefault();
+
+        // Log  amessage to the console
+        console.log('Submit Button clicked');
+    });
 }
